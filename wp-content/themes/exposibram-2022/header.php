7f54b08f9458a3d13d951275a0b40be7e7fbdcc5
Rogerio <rogerio@piloti.com.br>
2022-08-01 18:59:22 -0300

    fix: menu mobile

--- wp-content/themes/exposibram-2022/header.php
+++ wp-content/themes/exposibram-2022/header.php
@@ -32,19 +32,17 @@
 		<a class="skip-link screen-reader-text d-none" href="#primary"><?php esc_html_e('Skip to content', 'exposibram-2022'); ?></a>
 
 		<header id="masthead" class="site-header">
-			<div class="container">
+			<div class="container header-container">
 				<div class="row">
 					<div class="col-12">
-						<nav id="site-navigation" class="main-navigation menu-expo">
-							<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'exposibram-2022'); ?></button>
-
+						<nav class="main-navigation menu-expo">
 							<?php $currentlang = get_bloginfo('language');
 							if ($currentlang == "pt-BR") : ?>
 								<?php wp_nav_menu(array('menu' => 'Menu Header PT')); ?>
 							<?php elseif ($currentlang == "en-US") : ?>
 								<?php wp_nav_menu(array('menu' => 'Menu Header EN')); ?>
 							<?php endif; ?>
-							
+
 							<div class="lang">
 								<ul>
 									<?php pll_the_languages(); ?>
@@ -54,5 +52,28 @@
 					</div>
 				</div>
 			</div>
-
 		</header>
+
+		<!-- menu mobile -->
+		<div class="menu-mobile">
+			<nav id="site-navigation" class="main-navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+					<span></span>
+					<span></span>
+					<span></span>
+				</button>
+
+				<?php $currentlang = get_bloginfo('language');
+				if ($currentlang == "pt-BR") : ?>
+					<?php wp_nav_menu(array('menu' => 'Menu Header PT')); ?>
+				<?php elseif ($currentlang == "en-US") : ?>
+					<?php wp_nav_menu(array('menu' => 'Menu Header EN')); ?>
+				<?php endif; ?>
+
+				<div class="lang">
+					<ul>
+						<?php pll_the_languages(); ?>
+					</ul>
+				</div>
+			</nav>
+		</div>
