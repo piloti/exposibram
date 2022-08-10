74332281aa855b18a2b09298a4a823b18a6edc80
Rogerio <rogerio@piloti.com.br>
2022-08-10 16:35:18 -0300

    fix: add Google Tag Manager

--- wp-content/themes/exposibram-2022/header.php
+++ wp-content/themes/exposibram-2022/header.php
@@ -24,6 +24,14 @@
 	<script src="<?php echo get_bloginfo('template_directory'); ?>/dist/js/bootstrap.min.js"></script>
 	<script src="<?php echo get_bloginfo('template_directory'); ?>/dist/js/owl.carousel.min.js"></script>
 
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-NZTLSM4');</script>
+	<!-- End Google Tag Manager -->
+
 	<!-- Facebook Pixel Code -->
 	<script>
 		!function(f,b,e,v,n,t,s)
@@ -43,6 +51,11 @@
 </head>
 
 <body <?php body_class(); ?>>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NZTLSM4"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+
 	<?php wp_body_open(); ?>
 	<div id="page" class="site">
 		<a class="skip-link screen-reader-text d-none" href="#primary"><?php esc_html_e('Skip to content', 'exposibram-2022'); ?></a>
