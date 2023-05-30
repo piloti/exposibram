09c38036d7a418bdbadaf0a05d0ece06efd4aaf4
Rogerio <rogerio@piloti.com.br>
2023-05-30 18:37:12 -0300

    fix: some com patrocinio quando estive vazio

--- wp-content/themes/exposibram-2022/template-parts/patrocinadores.php
+++ wp-content/themes/exposibram-2022/template-parts/patrocinadores.php
@@ -73,21 +73,28 @@
           </div>
         </div>
 
-        <div class="row mb-4 mb-lg-5">
-          <div class="col-12">
-            <?php $currentlang = get_bloginfo('language');
-            if ($currentlang == "pt-BR") : ?>
-              <h4><strong>Gestão de Resíduos</strong></h4>
-            <?php elseif ($currentlang == "en-US") : ?>
-              <h4><strong>Waste Management</strong></h4>
-            <?php endif; ?>
-            <div class="align-items-center">
-              <?php foreach (get_field('add_gresiduos', 'options') as $key => $value) : ?>
-                <a href="<?= $value['link'] ?>" target="_blank" rel="noopener noreferrer" title="Gestão de Resíduos" class="d-inline-block"><img src="<?= $value['logo'] ?>" alt="Gestão de Resíduos"></a>
-              <?php endforeach; ?>
+        <?php
+        $currentlang = get_bloginfo('language');
+        $add_gresiduos = get_field('add_gresiduos', 'options');
+        if ($add_gresiduos) :
+        ?>
+          <div class="row mb-4 mb-lg-5">
+            <div class="col-12">
+              <?php if ($currentlang == "pt-BR") : ?>
+                <h4><strong>Gestão de Resíduos</strong></h4>
+              <?php elseif ($currentlang == "en-US") : ?>
+                <h4><strong>Waste Management</strong></h4>
+              <?php endif; ?>
+              <div class="align-items-center">
+                <?php
+                foreach ($add_gresiduos as $key => $value) :
+                ?>
+                  <a href="<?= $value['link'] ?>" target="_blank" rel="noopener noreferrer" title="Gestão de Resíduos" class="d-inline-block"><img src="<?= $value['logo'] ?>" alt="Gestão de Resíduos"></a>
+                <?php endforeach; ?>
+              </div>
             </div>
           </div>
-        </div>
+        <?php endif; ?>
 
         <div class="row mb-4 mb-lg-5">
           <div class="col-12">
